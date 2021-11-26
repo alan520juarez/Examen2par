dca75bdd5aebf050fc004f823f6f43a0254fb88e
Alanestrada225 <alan_estrada@ucol.mx>
2021-11-26 17:01:13 -0600

    first commit

---
+++ app/data/QuizData.js
@@ -0,0 +1,27 @@
+export default data = [
+    {
+        question: "Que comando sirve para instalar las depencias de un documento?",
+        options: ["npm install","nxp install","i install","npm react-native-install"],
+        correct_option: "npm install"
+    },
+    {
+        question: "Que comando se utiliza para correr un APPI?",
+        options: ["npm run-android","npx react-native run-android","run-react-native"],
+        correct_option: "npx react-native run-android"
+    },
+    {
+        question: "Comando para crear una APPI?",
+        options: ["create-app","app-create","create-react-native-app","npx react-native init NombreAPP"],
+        correct_option: "npx react-native init NombreAPP"
+    },
+    {
+        question: "Donde programar React Native?",
+        options: ["Cordova Apache","Visual Studio Code","Androi Studio","Ubunut"],
+        correct_option: "Visual Studio Code"
+    },
+    {
+        question: "Qué lenguaje de programación usa React Native?",
+        options: ["JavaScript","C++","Python","SQL"],
+        correct_option: "JavaScript"
+    }
+]
